46c51e8f96fc0e118de79f49abacc29a910baf5a
Yicong Liu <richardlyc94@gmail.com>
2018-04-17 17:46:58 -0400

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -1,5 +1,5 @@
 <?php
-    $db = mysqli_connect('localhost', 'root', 'suar1508', 'hw3_library');
+    $db = mysqli_connect('localhost', 'root', '********', 'hw3_library');
     if (mysqli_connect_errno()) {
         echo 'Database connection failed: '. mysqli_connect_error();
         die();
